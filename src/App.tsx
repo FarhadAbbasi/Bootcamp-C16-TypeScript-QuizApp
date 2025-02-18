028306021bd9c81feffc8abc0294ca6f59724ab1
Farhad Ali Abbasi <34246950+FarhadAbbasi@users.noreply.github.com>
2025-02-18 16:50:23 +0500

    1st commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// https://opentdb.com/api.php?amount=10&category=17&type=multiple
+
+function App() {
+  return (
+    <div>
+      Hello 
+    </div>
+  );
+}
+
+export default App;
